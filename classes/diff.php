912b0ad8524119f451f3d0871e6e34f60bacd8ec
justinhunt <bitwalkerjapan@gmail.com>
2018-12-09 21:13:18 +0900

    regex tweak

--- classes/diff.php
+++ classes/diff.php
@@ -64,7 +64,9 @@ class diff{
 
         //remove punctuation
         //see https://stackoverflow.com/questions/5233734/how-to-strip-punctuation-in-php
-        $thetext = preg_replace("[[:punct:]]", "", $thetext);
+       // $thetext = preg_replace("#[[:punct:]]#", "", $thetext);
+        //https://stackoverflow.com/questions/5689918/php-strip-punctuation
+        $thetext = preg_replace("/[:punct:]+/", "", $thetext);
 
         return $thetext;
     }
