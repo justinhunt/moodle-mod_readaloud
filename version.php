4a897a063ac9a166238b03203da8e83582aedca6
justinhunt <bitwalkerjapan@gmail.com>
2023-11-20 02:37:20 +0000

    version bump

--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023111200;
+$plugin->version = 2023111800;
 $plugin->requires = 2016052300;      // Requires Moodle 3.1
 $plugin->component = 'mod_readaloud';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.1.2 (Build 2023111200)';
+$plugin->release = '2.1.3 (Build 2023111800)';
