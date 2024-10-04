602eec579677714c4cf46daa80af9936f8e74da6
Justin Hunt <bitwalkerjapan@gmail.com>
2024-10-04 22:19:54 +0900

    version bump

--- developer.php
+++ developer.php
@@ -1,5 +1,4 @@
 <?php
-
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -27,8 +26,8 @@
 
 require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
 
-use \mod_readaloud\constants;
-use \mod_readaloud\utils;
+use mod_readaloud\constants;
+use mod_readaloud\utils;
 
 $id = optional_param('id', 0, PARAM_INT); // course_module ID, or
 $n  = optional_param('n', 0, PARAM_INT);  // ReadAloud instance ID
@@ -40,27 +39,25 @@ $action = optional_param('action', 'none', PARAM_TEXT); // report type
 
 if ($id) {
     $cm         = get_coursemodule_from_id(constants::M_MODNAME, $id, 0, false, MUST_EXIST);
-    $course     = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
-    $moduleinstance  = $DB->get_record(constants::M_TABLE, array('id' => $cm->instance), '*', MUST_EXIST);
-} elseif ($n) {
-    $moduleinstance  = $DB->get_record(constants::M_TABLE, array('id' => $n), '*', MUST_EXIST);
-    $course     = $DB->get_record('course', array('id' => $moduleinstance->course), '*', MUST_EXIST);
+    $course     = $DB->get_record('course', ['id' => $cm->course], '*', MUST_EXIST);
+    $moduleinstance  = $DB->get_record(constants::M_TABLE, ['id' => $cm->instance], '*', MUST_EXIST);
+} else if ($n) {
+    $moduleinstance  = $DB->get_record(constants::M_TABLE, ['id' => $n], '*', MUST_EXIST);
+    $course     = $DB->get_record('course', ['id' => $moduleinstance->course], '*', MUST_EXIST);
     $cm         = get_coursemodule_from_instance(constants::M_TABLE, $moduleinstance->id, $course->id, false, MUST_EXIST);
 } else {
-    print_error(0,'You must specify a course_module ID or an instance ID');
+    print_error(0, 'You must specify a course_module ID or an instance ID');
 }
 
 $PAGE->set_url(constants::M_URL . '/developer.php',
-	array('id' => $cm->id,'action'=>$action));
+    ['id' => $cm->id, 'action' => $action]);
 require_login($course, true, $cm);
 $modulecontext = context_module::instance($cm->id);
 
 require_capability('mod/readaloud:manage', $modulecontext);
 
-//Get an admin settings 
+// Get an admin settings
 $config = get_config(constants::M_COMPONENT);
-
-
 
 
 /// Set up the page header
@@ -72,28 +69,28 @@ $PAGE->requires->jquery();
 
 
 
-//This puts all our display logic into the renderer.php files in this plugin
+// This puts all our display logic into the renderer.php files in this plugin
 $renderer = $PAGE->get_renderer(constants::M_COMPONENT);
 $mode = 'view';
 $header = $renderer->header($moduleinstance, $cm, $mode, null, get_string('developer', constants::M_COMPONENT));
 
-//Process Actions.
+// Process Actions.
 
 switch ($action){
 
 
-	case 'generatedata':
+    case 'generatedata':
 
         $attempts = $DB->get_records(constants::M_USERTABLE,
-            array('readaloudid'=>  $moduleinstance->id),'timemodified DESC','*',0,1);
-	    if(!$attempts){
-            echo $header;
-	        echo '<h3>No attempt to generate data from</h3>';
-            echo $renderer->footer();
-            return;
+            ['readaloudid' => $moduleinstance->id], 'timemodified DESC', '*', 0, 1);
+        if(!$attempts){
+               echo $header;
+            echo '<h3>No attempt to generate data from</h3>';
+               echo $renderer->footer();
+               return;
         }else{
-	        $latestattempt = array_shift($attempts);
-            $ai = $DB->get_record(constants::M_AITABLE, array('attemptid'=>$latestattempt->id));
+            $latestattempt = array_shift($attempts);
+            $ai = $DB->get_record(constants::M_AITABLE, ['attemptid' => $latestattempt->id]);
             if(!$ai) {
                 echo $header;
                 echo '<h3>No AI for the latest attempt</h3>';
@@ -102,56 +99,57 @@ switch ($action){
             }
 
 
-	        $users = get_enrolled_users($modulecontext);
-	        //reindex array
-	        $users = array_values($users);
-	        $created = 0;
-	        for($x=0;$x<count($users);$x++){
-                copyAttempt($latestattempt,$ai,$users[$x]);
+            $users = get_enrolled_users($modulecontext);
+            // reindex array
+            $users = array_values($users);
+            $created = 0;
+            for($x = 0; $x < count($users); $x++){
+                copyAttempt($latestattempt, $ai, $users[$x]);
 
             }//end of user loop
             redirect(new \moodle_url(constants::M_URL . '/developer.php',
-                array('id' => $cm->id)),'Created Attempts:' . $created);
+            ['id' => $cm->id]), 'Created Attempts:' . $created);
 
         }
 
-		return;
+        return;
 
-	case 'none':
+    case 'none':
     default:
 }
 
-//output the page
+// output the page
 echo $header;
 
 echo "<div>Generate random attempts from the last attempt in the table, 1 for each enrolled user</div>";
-$sb= new \single_button(
-    new \moodle_url(constants::M_URL . '/developer.php', array('action' => 'generatedata', 'id' => $cm->id, 'n' => $moduleinstance->id)),
+$sb = new \single_button(
+    new \moodle_url(constants::M_URL . '/developer.php', ['action' => 'generatedata', 'id' => $cm->id, 'n' => $moduleinstance->id]),
     "Generate Attempt Data", 'get');
 echo $OUTPUT->render($sb);
 
 echo $renderer->footer();
 
-function copyAttempt($attempt, $ai, $user ){
+function copyattempt($attempt, $ai, $user ) {
     global $DB;
     $newatt = $attempt;
     $newai = $ai;
 
-    //attempt
+    // attempt
     $newatt->id = null;
     $newatt->timemodified = time();
     $newatt->timecreated = time();
-    $newatt->userid=$user->id;
-    $attemptid = $DB->insert_record(constants::M_USERTABLE,$newatt);
+    $newatt->userid = $user->id;
+    $attemptid = $DB->insert_record(constants::M_USERTABLE, $newatt);
     if(!$attemptid){return false;}
 
-    //ai
+    // ai
     $newai->id  = null;
     $newai->timemodified = time();
-    $newai->attemptid=$attemptid;
-    $aiid = $DB->insert_record(constants::M_AITABLE,$newai);
-    if(!$aiid){return false;}
+    $newai->attemptid = $attemptid;
+    $aiid = $DB->insert_record(constants::M_AITABLE, $newai);
+    if(!$aiid){return false;
+    }
 
-    //return true
+    // return true
     return true;
 }
--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024092500;
+$plugin->version = 2024100400;
 $plugin->requires = 2016052300;      // Requires Moodle 3.1
 $plugin->component = 'mod_readaloud';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.1.16 (Build 2024092500)';
+$plugin->release = '2.1.17 (Build 2024100400)';
