e486789eed9fa168baa5706e5bbef306ad35bdb9
Joanna Beaver <jobeaveris@hotmail.com>
2025-06-23 15:12:37 +0100

    Migrate styles to scss

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+// Defining requirements
+const { src, dest } = require('gulp');
+// var sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+
+// Run:
+// gulp sass
+// Compiles SCSS files in CSS
+gulp.task( 'sass', function() {
+    return src('./scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('.'));
+});
+
+
+// Run:
+// gulp watch
+// Starts watcher. Watcher runs gulp sass task on changes
+gulp.task( 'watch', function() {
+    gulp.watch('./scss/**/*.scss', gulp.series('sass'));
+});
