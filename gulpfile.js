c5c267e39008defb14d0d57a5047036363439c80
Joanna Beaver <jobeaveris@hotmail.com>
2025-07-20 11:47:31 +0100

    View template architecture, and styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,25 @@
-// gulpfile.js
-// Place this in moodle/mod/readaloud/
+// Defining requirements
+const { src, dest } = require('gulp');
+// var sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
 
-const { src, dest, task, watch, series } = require('gulp');
-const sass       = require('gulp-sass')(require('sass'));
-const sourcemaps = require('gulp-sourcemaps');
-const shell      = require('gulp-shell');
-const path       = require('path');
-
-// ─────────────────────────────────────────────────────────────────────────────
-// 1) Compute your project root (poodll5), three levels up from here:
-//    /…/poodll5/moodle/mod/readaloud → /…/poodll5
-// ─────────────────────────────────────────────────────────────────────────────
-const projectRoot   = path.resolve(__dirname, '../../../');
-
-// ─────────────────────────────────────────────────────────────────────────────
-// 2) Locate the Docker-compose wrapper under bin/
-// ─────────────────────────────────────────────────────────────────────────────
-const composeWrapper = path.join(projectRoot, 'bin', 'moodle-docker-compose');
-
-// ─────────────────────────────────────────────────────────────────────────────
-// 3) Compile SCSS → CSS
-// ─────────────────────────────────────────────────────────────────────────────
-task('sass', () =>
-  src('./scss/**/*.scss')
+// Run:
+// gulp sass
+// Compiles SCSS files in CSS
+gulp.task( 'sass', function() {
+    return src('./scss/**/*.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass())
     .pipe(sourcemaps.write('.'))
-    .pipe(dest('.'))
-);
+    .pipe(dest('.'));
+});
 
-// ─────────────────────────────────────────────────────────────────────────────
-// 4) Purge caches inside Docker
-//
-//    We run the wrapper *with* cwd=projectRoot so it finds .env and sets
-//    $MOODLE_DOCKER_WWWROOT properly.
-// ─────────────────────────────────────────────────────────────────────────────
-task('decache', shell.task(
-  [
-    // full command we want to run:
-    // /…/poodll5/bin/moodle-docker-compose exec webserver php admin/cli/purge_caches.php
-    `${composeWrapper} exec webserver php admin/cli/purge_caches.php --theme`
-  ],
-  {
-    cwd: projectRoot,
-    verbose: true
-  }
-));
 
-// ─────────────────────────────────────────────────────────────────────────────
-// 5) Composite tasks
-// ─────────────────────────────────────────────────────────────────────────────
-task('compile', series('sass', 'decache'));
-task('watch',   ()     => watch('./scss/**/*.scss', series('compile')));
-task('default', series('compile', 'watch'));
+// Run:
+// gulp watch
+// Starts watcher. Watcher runs gulp sass task on changes
+gulp.task( 'watch', function() {
+    gulp.watch('./scss/**/*.scss', gulp.series('sass'));
+});
