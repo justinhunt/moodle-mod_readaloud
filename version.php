b6c30296372649cf4f4e8795aad1feeeb8ed455e
Justin Hunt <bitwalkerjapan@gmail.com>
2026-09-25 20:01:36 +0900

    notes ahead of version bump

--- version.php
+++ version.php
@@ -29,4 +29,4 @@ $plugin->version = 2026092500;
 $plugin->requires = 2025041400;      // Requires Moodle 5.0
 $plugin->component = 'mod_readaloud';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.1.34 (Build 2026092500)';
+$plugin->release = '2.1.31 (Build 2026092500)';
